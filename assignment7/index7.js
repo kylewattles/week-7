bc688494910f56f2182fa47b8b3355da04d934b3
kyle wattles <wattles.kyle66@Gmail.com>
2024-12-23 16:07:47 -0800

    adding stuff i forgot in the last push

--- assignment7/index7.js
+++ assignment7/index7.js
@@ -122,12 +122,22 @@ function santaClaus (word, n){
 }
  santaClaus("Ho!", 3);  
  
- /*Write a function that takes two parameters, firstName and lastName, and returns a full name. The full name should be the first and the last name separated by a space.*/ 
+ /*
+ 8. Write a function that takes two parameters, firstName and lastName, and returns a full name. The full name should be the first and the last name separated by a space.*/ 
 
 function fullName(firstName,lastName){
     return firstName + " " + lastName
 }
 console.log(fullName("Kyle", "Wattles" ));
+
+/*
+9. Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.*/
+
+const numbersArray = [28, 42, 69,]
+function isSumGreaterThanOneHundred[]{
+    
+} 
+
 
 
 
